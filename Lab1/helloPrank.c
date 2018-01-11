998b593aef4322e073864793cc8fe5347a647901
Dylan Wright <dylmwrig@umail.iu.edu>
2018-01-10 22:06:55 -0500

    Done with the prank portion of the assignment

--- Lab1/helloPrank.c
+++ Lab1/helloPrank.c
@@ -2,13 +2,20 @@
 #include <unistd.h>
 #include <string.h>
 
+/*
+ * the original idea was to have multiple files saying "HI" on a loop at the same time
+ * while new ones were created and executed
+ * I didn't consider how control was transferred and that only one would run at a time
+ */
 int main()
 {
   char fileName[50];
   char origName[50];
   char fileModStr[10];
+  char outName[50];
+  char cmd[50];
   int fileMod = 1;
-  origName[0] = 'h';
+  origName[0] = 'h'; //both the output file and the new source files will start with "hi"
   origName[1] = 'i';
   FILE * fptr;
     
@@ -19,9 +26,25 @@ int main()
     sprintf(fileModStr, "%d", fileMod);
     strcat(fileName, fileModStr);
     strcat(fileName, ".c");
-    fptr = fopen(fileName, "w");
-    sleep(3);
+    fptr = fopen(fileName, "w"); //create a new source file
+    fprintf(fptr, "#include <stdio.h>\n"
+                  "int main(){"
+                  "printf(\"HI\");\n"
+                  "return 0;}");
     fclose(fptr);
+                
+    strcpy(outName, origName);
+    strcat(outName, fileModStr);
+    strcat(outName, ".out ");                     
+    strcpy(cmd, "gcc -o");
+    strcat(cmd, outName);
+    strcat(cmd, fileName);
+    system(cmd);       //create the output file using the newly created source
+    strcpy(cmd, "./"); //run the output file
+    strcat(cmd, outName);
+    system(cmd);
+
+    sleep(3);
     fileMod++;
   } //end while
   return 0;
