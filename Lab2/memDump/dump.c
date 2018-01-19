d0c41b74628ba69d61d25f355cc546dc954e53a4
Dylan Wright <dylmwrig@umail.iu.edu>
2018-01-18 22:07:56 -0500

    Pretty much done, just not submitting yet. My comments on a couple files are pretty sparse

--- Lab2/memDump/dump.c
+++ Lab2/memDump/dump.c
@@ -1,8 +1,57 @@
 void dump_memory(void *p, int len)
 {
   int i;
+  printf("address\t\tchar\thexCh\tshort\tinteger\t\tfloat\t\tdoubleFloat\n");
 
   for (i = 0; i < len; i++) {
-    printf("%8p: %02x\n", p + i, *(unsigned char *)(p + i));
+    printf("%8p: ", p + i);
+    
+    //if ((isascii(*(unsigned char *) (p + i)) == 0) && *(char *)(p + i) < 126)
+    char c = *(unsigned char *)(p + i);
+    int ci = (int)c;
+    if ((ci >= 127) || (ci < 32))
+    {
+      printf("?");
+    }   
+   
+    else
+    {
+      printf("%c", c);//*(unsigned char *)(p + i));
+    } 
+    printf("\t0x%0x", *(unsigned char *)(p + i));
+    
+    //short
+    if (i%2 == 0)
+    {
+      printf("\t%+hi", *(short *)(p + i));
+    } //end if
+
+    else
+    {
+      printf("\t");
+    } //end else
+
+    //integer and float
+    if (i%4 == 0)
+    {
+      printf("\t%+d", *(int *)(p + i));
+      printf("\t%e", *(float *)(p + i));
+    } //end if
+
+    else
+    {
+      printf("\t\t");
+    } //end else
+
+    if (i%8 == 0)
+    {
+      printf("\t%g", *(double *)(p + i));
+    } //end if
+   
+    else
+    {
+      printf("\t");
+    } //end else
+    printf("\n");
   }
 }
