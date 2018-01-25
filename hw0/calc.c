47442f43ba2d5347bb21f75a60d3c56644e75845
Dylan Wright <dylmwrig@umail.iu.edu>
2018-01-24 22:39:54 -0500

    Calculations seem to be working

--- hw0/calc.c
+++ hw0/calc.c
@@ -6,31 +6,101 @@
 
 #include <stdio.h>
 
-struct Node
+//node for linked list
+typedef struct Node
 {
   int data;
   struct Node * next;
-}; //end Node
+} Node; //end Node
+
+//two methods for the linked list
+//return new pointer to current node
+//newN is the node being created
+//its made outside of the function because its a pointer
+//and if I initialized it inside of this function, it would point to nothing
+//I guess this doesn't look much like a push function anymore but this is how I decided to do it
+//
+//next is the node this will point to (tail node is initialized to NULL so no need to check if this is the first node)
+//i is data that new node is to be initialized with
+void push(Node * newN, Node * next, int i)
+{
+  newN->data = i;
+  newN->next = next;
+} //end push
+
+//delete the "head" node and return its value
+int pop(Node ** n)
+{
+  Node * curr = *n;
+  if (curr == NULL)
+  {
+    printf("Not enough operands!\n"); //if pop was called too much, that means the user used an operator with less than 2 operands on the stack
+    return;
+  } //end if
+
+  else
+  {
+    int rtn = curr->data;
+    Node * old = curr;
+    *n = curr->next;
+    free(old); 
+  
+    return rtn;
+  } //end eles
+} //end pop
 
 //call when q or c is entered as a command
-void clearStack(struct Node * head)
+void clearStack(Node * n)
 {
-  if (head == NULL)
+  while (n != NULL)
   {
-    return;
+    
   } //end if
 } //end clearStack
 
-void applyOp(char op)
+//apply operator to the first two elements in the list
+//return the value for the node which will be pushed after the operation is complete
+int applyOp(Node ** n, char op)
 {
+  int rhs = pop(n);
+  int lhs = pop(n);
 
+  if (op == '+')
+  {
+    return (lhs + rhs);
+  } //end if
+
+  else if (op == '-')
+  {
+    return (lhs - rhs);
+  } //end else if
+
+  else if (op == '*')
+  {
+    return (lhs * rhs);
+  } //end else if
+
+  //no real need for error checking here
+  //because the input is pretty controlled
+  else
+  {
+    return (lhs / rhs);
+  } //end else
 } //end applyOp
 
-void printList(struct Node * head)
+void printList(Node * n)
 {
-  while (head != NULL)
+  //kind of a redundant check
+  //but I'm keeping it in for error handling
+  if (n == NULL)
   {
-    printf("%d\n", head->
+    printf("Nothing to print!\n");
+  } //end if
+
+  while (n != NULL)
+  {
+    printf("%d\n", n->data);
+    n = n->next;
   } //end while
 } //end printList
 
@@ -39,10 +109,9 @@ int main()
   char c = ' ', input[100];
   int i;
 
-  //initialize the tail element, shouldn't point to anything
+  //initialize the tail element to be null
   //"tail" doesn't seem right in context of this being a stack, but it seems natural because it's a linked list
-  struct Node * tail = (node*)malloc(sizeof(Node));
-  tail->next = NULL;
+  Node * n = NULL;
 
   while (c != 'q')
   {
@@ -61,7 +130,15 @@ int main()
       //return top of stack, don't alter anything
       else if (c == 'p')
       {
+        if (n != NULL)
+        {
+          printf("%d\n", n->data);
+        } //end if
 
+        else
+        {
+          printf("Nothing to print!\n");
+        } //end else
       } //end else if
 
       //clear stack
@@ -73,19 +150,43 @@ int main()
       //print everything "top to bottom"
       else if (c == 'f')
       {
-
+        printList(n);
       } //end else if
 
-      //otherwise it is an operand
+      //otherwise it is an operator
       else
       {
-        applyOp(c);
+        Node * newN = (Node*)malloc(sizeof(Node));
+        if (newN)
+        {
+          Node ** p = &n;
+          i = applyOp(p, c);
+          push(newN, n, i);
+          n = newN;
+        } //end if
+
+        else
+        {
+          printf("Error creating node");
+        } //end else
       } //end else
     } //end if
 
     else
     {
       i = atoi(input);
+  
+      Node * newN = (Node*)malloc(sizeof(Node));
+      if (newN)
+      {      
+        push(newN, n, i);
+        n = newN;
+      } //end if
+  
+      else
+      {
+        printf("Error creating node");
+      } //end else
     } //end else
   } //end while
 
