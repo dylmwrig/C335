c485534d84b129edcd34dcbb22882cce8767905c
Dylan Wright <dylmwrig@umail.iu.edu>
2018-02-15 20:00:13 -0500

    Got the screen to become red! Good enough for today

--- driver/src/f3d_lcd_sd.c
+++ driver/src/f3d_lcd_sd.c
@@ -44,7 +44,6 @@ void f3d_lcd_sd_interface_init(void) {
   
   //pins 9 through 12
   RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
-  //RCC_AHBPeriphClockCmd(RCC_AHBPeriph_SPI2, ENABLE); //unsure between this line and the one above
   GPIO_InitTypeDef GPIO_InitStructure;
   GPIO_StructInit(&GPIO_InitStructure);
   GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10 | GPIO_Pin_11 | GPIO_Pin_12;
@@ -52,18 +51,22 @@ void f3d_lcd_sd_interface_init(void) {
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-  GPIO_Init(GPIOE, &GPIO_InitStructure);
 
-  //pins 13 14 and 15
-  GPIO_InitTypeDef GPIO_InitStructure;
+  GPIO_Init(GPIOB, &GPIO_InitStructure);
+
   GPIO_StructInit(&GPIO_InitStructure);
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15; | GPIO_Pin_12;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF; 
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-  GPIO_Init(GPIOE, &GPIO_InitStructure);
-  
+ 
+  GPIO_PinAFConfig(GPIOB, 13, GPIO_AF_5);
+  GPIO_PinAFConfig(GPIOB, 14, GPIO_AF_5);
+  GPIO_PinAFConfig(GPIOB, 15, GPIO_AF_5);
+
+  GPIO_Init(GPIOB, &GPIO_InitStructure);
+
   /* ^^^^^^^^^^^ pin initialization for the LCD goes here ^^^^^^^^^^ */
  
   // Section 4.1 SPI2 configuration
@@ -79,9 +82,9 @@ void f3d_lcd_sd_interface_init(void) {
   SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
   SPI_InitStructure.SPI_CRCPolynomial = 7;
   SPI_InitStructure.SPI_Mode = SPI_Mode_Master;
-  SPI_Init(/*something goes here*/, &SPI_InitStructure);
-  SPI_RxFIFOThresholdConfig(/*somthing goes here*/, SPI_RxFIFOThreshold_QF);
-  SPI_Cmd(/*something goes here*/, ENABLE);
+  SPI_Init(SPI2, &SPI_InitStructure);
+  SPI_RxFIFOThresholdConfig(SPI2, SPI_RxFIFOThreshold_QF);
+  SPI_Cmd(SPI2, ENABLE);
   
 } 
 
---
+++ lab6/main.c
@@ -0,0 +1,52 @@
+/* main.c 
+* Filename: main.c
+* Description:
+* * Author:
+* * Maintainer:
+* * Created: Thu Jan 10 11:23:43 2013
+* * Code: */
+
+#include <stm32f30x.h> // Pull in include files for F30x standard drivers
+#include <f3d_led.h> // Pull in include file for the local drivers
+#include <f3d_uart.h>
+#include <f3d_gyro.h>
+#include <f3d_lcd_sd.h>
+#include <stdio.h>
+
+#define TIMER 20000
+
+int main(void) {
+  // If you have your inits set up, this should turn your LCD screen red
+     f3d_lcd_init();
+     f3d_uart_init();
+  
+//     f3d_gyro_init();
+     f3d_user_btn_init();
+
+     setvbuf(stdin, NULL, _IONBF,0);
+     setvbuf(stdout, NULL, _IONBF,0);
+     setvbuf(stderr, NULL, _IONBF,0);
+
+
+
+     delay(100);
+     f3d_lcd_fillScreen(RED);
+
+
+     float gyroVal[3];
+/*
+     while(1){
+       f3d_gyro_getdata(gyroVal);
+     }
+*/
+}
+  
+#ifdef USE_FULL_ASSERT
+void assert_failed(uint8_t* file, uint32_t line) {
+/* Infinite loop */
+/* Use GDB to find out why we're here */
+  while (1);
+  }
+  #endif
+
+  /* main.c ends here */
