5981a687e6afbe7703b3f42c5dd154c89e0ecbd6
Dylan Wright <dylmwrig@umail.iu.edu>
2018-03-08 17:01:21 -0500

    Graph is looking pretty nice but not going into negative parts of the graph for some reason

--- lab7/main.c
+++ lab7/main.c
@@ -34,9 +34,9 @@ void graphInit(){
   //I tried doing this in a for loop and got a bug for some reason
   while(i < 100){
     f3d_lcd_drawPixel(i, 20, BLACK);
-    f3d_lcd_drawPixel(i, 70, BLACK);
+    f3d_lcd_drawPixel(i, 60, BLACK);
     f3d_lcd_drawPixel(i, 100, BLACK);
-    f3d_lcd_drawPixel(i, 150, BLACK);
+    f3d_lcd_drawPixel(i, 140, BLACK);
     i++;
   } //end while
 
@@ -44,8 +44,98 @@ void graphInit(){
   //so I'm just going to label the graphs
   
   f3d_lcd_drawChar(10, 40, '0', BLACK, WHITE);
-  f3d_lcd_drawChar(10, 40, '0', BLACK, WHITE);
+  f3d_lcd_drawChar(10, 120, '0', BLACK, WHITE);
+  f3d_lcd_drawString(60, 10, "PITCH", BLACK, WHITE);
+  f3d_lcd_drawString(60, 90, "ROLL", BLACK, WHITE);
 } //end graphInit
+
+//move each value in the array one to the left
+void swapVals(float pitch[80], float roll[80]){
+  int x;
+  for (x = 79; x > 0; x--){
+    pitch[x] = pitch[x-1];
+    roll[x] = roll[x-1];
+  } //end for
+} //end swapVals
+
+//first, move each index in the array one to the right
+//each previously used point will be passed in a two dimensional array
+//to move them left, just subtract the x values by 1
+//print values from the array in right to left
+//
+//if they reach the left border, swap indeces on the left end of the array
+//
+//final step is to
+//add the new value to the rightmost part of the graph.
+//
+//sorry this function is so hacky lol
+void updateGraphs(float oldPitch[80], float oldRoll[80], float newPitch, float newRoll){
+  int i, pitchY, rollY;
+
+  for (i = 0; i < 79; i++){
+    pitchY = (int)(oldPitch[i] * 10.0);
+    if (oldPitch[i] >= 0.0){
+      pitchY = 40 - pitchY;
+    } //end if
+
+    else{
+      pitchY += 40;
+    } //end else
+
+    f3d_lcd_drawPixel(i+20, pitchY, WHITE);
+  } //end for
+
+  for (i = 0; i < 79; i++){
+    rollY = (int)(oldRoll[i] * 10.0);
+    if (oldRoll[i] >= 0.0){
+      rollY = 120 - rollY;
+    } //end if
+
+    else{
+      rollY += 120;
+    } //end else
+
+    //printf("Roll Y: %f\n", rollY);
+
+    f3d_lcd_drawPixel(i+20, rollY, WHITE);
+  } //end for
+
+  swapVals(oldPitch, oldRoll);
+  oldPitch[0] = newPitch;
+  oldRoll[0] = newRoll;
+
+  //to scale the values to the graph, the upper and lower bounds will be 2.0 and -2.0
+  //there are 40 pixels vertically on each graph so each 0.1 corresponds to one pixel
+  for (i = 0; i < 79; i++){
+    pitchY = (int)(oldPitch[i] * 10.0);
+    if (oldPitch[i] >= 0.0){
+      pitchY = 40 - pitchY;
+    } //end if
+
+    else{
+      pitchY += 40;
+    } //end else
+
+    f3d_lcd_drawPixel(i+20, pitchY, BLACK);
+  } //end for
+
+  //to scale the values to the graph, the upper and lower bounds will be 2.0 and -2.0
+  //there are 40 pixels vertically on each graph so each 0.1 corresponds to one pixel
+  for (i = 0; i < 79; i++){
+    rollY = (int)(oldRoll[i] * 10.0);
+    if (oldRoll[i] >= 0.0){
+      rollY = 120 - rollY;
+    } //end if
+
+    else{
+      rollY += 120;
+    } //end else
+
+    f3d_lcd_drawPixel(i+20, rollY, BLACK);
+  } //end for
+
+
+} //end updateGraphs
 
 int main(void) {
   // Set up your inits before you go ahead  
@@ -67,33 +157,21 @@ int main(void) {
   setvbuf(stderr, NULL, _IONBF, 0);
 
   graphInit();
+  float pitchVals[80], rollVals[80];
 
   float accelVal[3], magVal[3]; //x, y, and z readings from accelerometer and magnetometer
   while(1){
-    /*
     f3d_accel_read(accelVal);
     f3d_mag_read(magVal);
  
     float pitch = atan2(accelVal[0], pow(accelVal[1], 2) + pow(accelVal[2], 2));
     float roll = atan2(accelVal[1], pow(accelVal[0], 2) + pow(accelVal[2], 2));
     float heading = atan2(magVal[1], magVal[0]);
-*/
-    //printf("Pitch and roll: %f %f\n", pitch, roll);
-    //printf("Heading: %f\n", heading);
 
-    //I would like to visualize pitch and roll in a way similar to heart rate ECG monitors
-    //where it sort of shows the "history" of the pitch and roll values 
-     
- 
-/*
- *  simple way to draw a line using lcd_drawPixel
-    int x, y;
-    for (x = 50; x < 150; x++){
-      y = x;
-      f3d_lcd_drawPixel(x, y, BLACK);
-    }
-*/
-  } //end while
+    printf("Pitch and roll: %f %f\n", pitch, roll);
+    //printf("Heading: %f\n", heading); 
+    updateGraphs(pitchVals, rollVals, pitch, roll);
+ } //end while
 } //end main
   
 #ifdef USE_FULL_ASSERT
