484c3a54e53db28d7a961824a09982ea6f23356d
Dylan Wright <dylmwrig@umail.iu.edu>
2018-03-08 15:35:09 -0500

    Working on visualization

--- lab7/main.c
+++ lab7/main.c
@@ -21,8 +21,31 @@
 #include <f3d_accel.h>
 #include <f3d_mag.h>
 #include <stdio.h>
+#include <math.h>
 
 #define TIMER 20000
+
+//initialize graphs for pitch and roll visualization
+void graphInit(){
+  f3d_lcd_fillScreen(WHITE);
+
+  //first draw the horizontal lines
+  int i = 20;
+  //I tried doing this in a for loop and got a bug for some reason
+  while(i < 100){
+    f3d_lcd_drawPixel(i, 20, BLACK);
+    f3d_lcd_drawPixel(i, 70, BLACK);
+    f3d_lcd_drawPixel(i, 100, BLACK);
+    f3d_lcd_drawPixel(i, 150, BLACK);
+    i++;
+  } //end while
+
+  //don't really need vertical lines now that I'm thinking about it
+  //so I'm just going to label the graphs
+  
+  f3d_lcd_drawChar(10, 40, '0', BLACK, WHITE);
+  f3d_lcd_drawChar(10, 40, '0', BLACK, WHITE);
+} //end graphInit
 
 int main(void) {
   // Set up your inits before you go ahead  
@@ -43,16 +66,33 @@ int main(void) {
   setvbuf(stdout, NULL, _IONBF, 0);
   setvbuf(stderr, NULL, _IONBF, 0);
 
+  graphInit();
+
   float accelVal[3], magVal[3]; //x, y, and z readings from accelerometer and magnetometer
   while(1){
+    /*
     f3d_accel_read(accelVal);
     f3d_mag_read(magVal);
+ 
+    float pitch = atan2(accelVal[0], pow(accelVal[1], 2) + pow(accelVal[2], 2));
+    float roll = atan2(accelVal[1], pow(accelVal[0], 2) + pow(accelVal[2], 2));
+    float heading = atan2(magVal[1], magVal[0]);
+*/
+    //printf("Pitch and roll: %f %f\n", pitch, roll);
+    //printf("Heading: %f\n", heading);
 
-    //first, calculate tilt angle
-    //tilt angle = pitch (x axis relative to horizontal plane) and roll (y axis relative to horizontal plane
-    //pitch = arctan(Ax1 / ((Ay1)^2 + (Az1)^2)
-    //roll = arctan(Ay1 / ((Ax1)^2 + (Az1)^2)
-    //)
+    //I would like to visualize pitch and roll in a way similar to heart rate ECG monitors
+    //where it sort of shows the "history" of the pitch and roll values 
+     
+ 
+/*
+ *  simple way to draw a line using lcd_drawPixel
+    int x, y;
+    for (x = 50; x < 150; x++){
+      y = x;
+      f3d_lcd_drawPixel(x, y, BLACK);
+    }
+*/
   } //end while
 } //end main
   
