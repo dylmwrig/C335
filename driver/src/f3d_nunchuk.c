397c39bf0362460775ba8b3a0cbd437c6b0a83f8
Dylan Wright <dylmwrig@umail.iu.edu>
2018-04-22 20:00:16 -0400

    Working on second nunchuk

--- driver/src/f3d_nunchuk.c
+++ driver/src/f3d_nunchuk.c
@@ -35,6 +35,7 @@
 /* Code: */
 
 #include <f3d_i2c.h>
+#include <f3d_i2c2.h>
 #include <f3d_nunchuk.h>
 #include <stdio.h>
 
@@ -45,12 +46,15 @@ void f3d_nunchuk_init(void) {
   uint8_t buf = 0x55;
   uint8_t buf2 = 0x00;
 
-
   // Use for factory Wii Nunchuk controllers
-  // f3d_i2c1_write(NUNCHUK_ADDRESS,0x40,&realb);
+  //f3d_i2c1_write(NUNCHUK_ADDRESS,0x40,&realb);
   // Use for 3 party nunchuks like we have in the lab
   f3d_i2c1_write(NUNCHUK_ADDRESS,0xf0,&buf); 
   f3d_i2c1_write(NUNCHUK_ADDRESS,0xfb,&buf2);
+
+// second nunchuk code (uncomment me to use!)
+  f3d_i2c2_write(NUNCHUK_ADDRESS,0xf0,&buf3);
+  f3d_i2c2_write(NUNCHUK_ADDRESS,0xfb,&buf4);
 }
 
 void f3d_nunchuk_read(nunchuk_t *n) {
@@ -59,6 +63,49 @@ void f3d_nunchuk_read(nunchuk_t *n) {
   unsigned short tmp;
 
   f3d_i2c1_read_nunchuk(0xA5, data, 6);
+  n->jx = data[0];
+  n->jy = data[1];
+
+  tmp = data[2];
+  tmp = tmp << 2;
+  n->ax = tmp | ((data[5] & 0x0C) >> 2);
+
+  tmp = data[3];
+  tmp = tmp << 2;
+  n->ay = tmp | ((data[5] & 0x30) >> 4);
+
+  tmp = data[4];
+  tmp = tmp << 2;
+  n->az = tmp | ((data[5] & 0xC0) >> 6);
+
+  switch (data[5] & 0x03) {
+  case 2:
+    n->z = 1;
+    n->c = 0;
+    break;
+  case 1:
+    n->z = 0;
+    n->c = 1;
+    break;
+  case 0:
+    n->z = 1;
+    n->c = 1;
+    break;
+  case 3:
+    n->z = 0;
+    n->c = 0;
+    break;
+  }
+}
+
+//read function for the second nunchuk
+//only difference is that f3d_i2c1_read_nunchuk is now i2c2
+void f3d_nunchuk_read2(nunchuk_t *n) {
+  uint8_t data[6] = {0,0,0,0,0,0};
+  int i;
+  unsigned short tmp;
+
+  f3d_i2c2_read_nunchuk(0xA5, data, 6);
   n->jx = data[0];
   n->jy = data[1];
 
